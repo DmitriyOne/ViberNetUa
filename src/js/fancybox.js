3eb629fa08478cda4591a734c01581911853648a
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2021-04-10 10:44:06 +0300

    block review

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -2,8 +2,148 @@ import '@fancyapps/fancybox';
 
 
 $('[data-fancybox]').fancybox({
-
+  
 });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// первая
+$('.slider__overlay-01').mouseover(function() {
+  $('.slide__border-01').toggleClass('active');
+  $('.slider__name-document-01').toggleClass('active');
+});
+
+
+$('.slider__overlay-01').mouseout(function() {
+  $('.slide__border-01').removeClass('active');
+  $('.slider__name-document-01').removeClass('active');
+});
+
+// вторая
+$('.slider__overlay-02').mouseover(function() {
+  $('.slide__border-02').toggleClass('active');
+  $('.slider__name-document-02').toggleClass('active');
+});
+
+
+$('.slider__overlay-02').mouseout(function() {
+  $('.slide__border-02').removeClass('active');
+  $('.slider__name-document-02').removeClass('active');
+});
+// третий
+$('.slider__overlay-03').mouseover(function() {
+  $('.slide__border-03').toggleClass('active');
+  $('.slider__name-document-03').toggleClass('active');
+});
+
+
+$('.slider__overlay-03').mouseout(function() {
+  $('.slide__border-03').removeClass('active');
+  $('.slider__name-document-03').removeClass('active');
+});
+// четвертый
+$('.slider__overlay-04').mouseover(function() {
+  $('.slide__border-04').toggleClass('active');
+  $('.slider__name-document-04').toggleClass('active');
+});
+
+
+$('.slider__overlay-04').mouseout(function() {
+  $('.slide__border-04').removeClass('active');
+  $('.slider__name-document-04').removeClass('active');
+});
+// пятый
+$('.slider__overlay-05').mouseover(function() {
+  $('.slide__border-05').toggleClass('active');
+  $('.slider__name-document-05').toggleClass('active');
+});
+
+
+$('.slider__overlay-05').mouseout(function() {
+  $('.slide__border-05').removeClass('active');
+  $('.slider__name-document-05').removeClass('active');
+});
+// шестой
+$('.slider__overlay-06').mouseover(function() {
+  $('.slide__border-06').toggleClass('active');
+  $('.slider__name-document-06').toggleClass('active');
+});
+
+
+$('.slider__overlay-06').mouseout(function() {
+  $('.slide__border-06').removeClass('active');
+  $('.slider__name-document-06').removeClass('active');
+});
+// семь
+$('.slider__overlay-07').mouseover(function() {
+  $('.slide__border-07').toggleClass('active');
+  $('.slider__name-document-07').toggleClass('active');
+});
+
+
+$('.slider__overlay-07').mouseout(function() {
+  $('.slide__border-07').removeClass('active');
+  $('.slider__name-document-07').removeClass('active');
+});
+// восем
+$('.slider__overlay-08').mouseover(function() {
+  $('.slide__border-08').toggleClass('active');
+  $('.slider__name-document-08').toggleClass('active');
+});
+
+
+$('.slider__overlay-08').mouseout(function() {
+  $('.slide__border-08').removeClass('active');
+  $('.slider__name-document-08').removeClass('active');
+});
+// девять
+$('.slider__overlay-09').mouseover(function() {
+  $('.slide__border-09').toggleClass('active');
+  $('.slider__name-document-09').toggleClass('active');
+});
+
+
+$('.slider__overlay-09').mouseout(function() {
+  $('.slide__border-09').removeClass('active');
+  $('.slider__name-document-09').removeClass('active');
+});
+// десять
+$('.slider__overlay-10').mouseover(function() {
+  $('.slide__border-10').toggleClass('active');
+  $('.slider__name-document-10').toggleClass('active');
+});
+
+
+$('.slider__overlay-10').mouseout(function() {
+  $('.slide__border-10').removeClass('active');
+  $('.slider__name-document-10').removeClass('active');
+});
+// одинацать
+$('.slider__overlay-11').mouseover(function() {
+  $('.slide__border-11').toggleClass('active');
+  $('.slider__name-document-11').toggleClass('active');
+});
+
+
+$('.slider__overlay-11').mouseout(function() {
+  $('.slide__border-11').removeClass('active');
+  $('.slider__name-document-11').removeClass('active');
+});
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,8 +4,8 @@ import Swiper from 'swiper/bundle';
 
 new Swiper('.swiper-review', {
   allowTouchMove: false,
-  slidesPerView: 3,
-  spaceBetween: 10,
+  slidesPerView: 1,
+  spaceBetween: 0,
   loop: true,
   speed: 700,
   pagination: {
@@ -22,6 +22,16 @@ new Swiper('.swiper-review', {
     nextEl: '.button-next__review, .arrows__imgR',
     prevEl: '.button-prev__review, .arrows__imgL',
   },
+  breakpoints: {
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    620: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
 });
 
 
