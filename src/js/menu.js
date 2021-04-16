7c7208b36b2bac69dae6250efe7a03a6eaee1158
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2021-04-16 16:15:13 +0300

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -22,12 +22,12 @@ $('.header-btn').on('click', function() {
 
 });
 
-var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-  $page.animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 2000);
-  return false;
+
+$('.menu').on('click','a', function(event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
 });
 
 $('.menu__link').click(function() {
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,33 +1,39 @@
-// попапы
-// $('.hello-popup').click(function(event) {
-//   event.preventDefault();
-//   show();
-// });
-
-// $('.popup__bg').click(function(event) {
-//   event.preventDefault();
-//   hide();
-// });
-
 $('.zayavka').click(function(event) {
-  event.preventDefault();
   show();
 });
 
-$('.popup__bg').click(function(event) {
-  event.preventDefault();
+$('.popup-first__bg').click(function(event) {
   hide();
 });
 
 
-// функции для попапов
-// открытие
+
 function show() {
+  $('.popup-first').fadeIn(300);
+  $('.popup-first__bg').fadeIn(300);
+}
+function hide() {
+  $('.popup-first').fadeOut(300);
+  $('.popup-first__bg').fadeOut(300);
+}
+
+$('.zayavka-popup-first').click(function(event) {
+  event.preventDefault();
+  show1();
+});
+
+$('.popup__bg').click(function(event) {
+  event.preventDefault();
+  hide1();
+});
+
+
+
+function show1() {
   $('.popup').fadeIn(300);
   $('.popup__bg').fadeIn(300);
 }
-// закрытие
-function hide() {
+function hide1() {
   $('.popup').fadeOut(300);
   $('.popup__bg').fadeOut(300);
 }
